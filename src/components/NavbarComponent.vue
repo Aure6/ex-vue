c24d24fafb488c3aafd71c09dd5ba0f69f522f80
Aure6 <aurelien7112001@gmail.com>
2023-11-27 15:22:47 +0100

    mobile menu button added to a div with both ul

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -11,33 +11,36 @@ const isOpen = ref(false)
     <nav class="bg-gray-800 p-4">
         <div class="container mx-auto flex items-center justify-between">
             <router-link to="/" class="text-white text-lg font-bold">Mon Site</router-link>
+            <div>
+                <!-- Mobile menu button -->
+                <button class="text-white md:hidden  " @click="isOpen = !isOpen">
+                    {{ isOpen ? 'Fermer' : 'Ouvrir' }}
+                    <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                        <path fill-rule="evenodd"
+                            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                        </path>
+                    </svg>
+                </button>
 
-            <!-- Mobile menu button -->
-            <button class="text-white md:hidden  " @click="isOpen = !isOpen">
-                {{ isOpen ? 'Fermer' : 'Ouvrir' }}
-                <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                    <path fill-rule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
-                    </path>
-                </svg>
-            </button>
-
-            <ul v-if="isOpen" class="flex flex-col space-x-4 text-white md:hidden">
-                <h2>Menu mobile</h2>
-                <router-link to="/">Accueil</router-link>
-                <router-link to="/about">À Propos</router-link>
-                <router-link to="/services">Services</router-link>
-                <router-link to="/contact">Contact</router-link>
-            </ul>
+                <ul v-if="isOpen" class="flex flex-col space-x-4 text-white md:hidden">
+                    <h2>Menu mobile</h2>
+                    <router-link to="/">Accueil</router-link>
+                    <router-link to="/about">À Propos</router-link>
+                    <router-link to="/services">Services</router-link>
+                    <router-link to="/contact">Contact</router-link>
+                </ul>
 
 
-            <ul v-else class="hidden md:flex space-x-4 text-white  md:visible ">
-                <h2>Menu large</h2>
-                <router-link to="/">Accueil</router-link>
-                <router-link to="/about">À Propos</router-link>
-                <router-link to="/services">Services</router-link>
-                <router-link to="/contact">Contact</router-link>
-            </ul>
+                <ul v-else class="hidden md:flex space-x-4 text-white  md:visible ">
+                    <h2>Menu large</h2>
+                    <router-link to="/">Accueil</router-link>
+                    <router-link to="/about">À Propos</router-link>
+                    <router-link to="/services">Services</router-link>
+                    <router-link to="/contact">Contact</router-link>
+                </ul>
+            </div>
+
+
         </div>
     </nav>
 </template>
