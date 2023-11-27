313c78c431009607bac106ec66825d9da551c71f
Aure6 <aurelien7112001@gmail.com>
2023-11-27 15:26:09 +0100

    Update App.vue: remove HelloWorld component, headings added

--- src/App.vue
+++ src/App.vue
@@ -12,29 +12,18 @@ import ContactFormComponent from '/src/components/ContactFormComponent.vue'
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <NavbarComponent />
-
-    <!-- <div class="wrapper">
-                      <HelloWorld msg="You did it!" />
-                      <nav>
-                        <RouterLink to="/">Home</RouterLink>
-                        <RouterLink to="/about">About</RouterLink>
-                      </nav>
-                    </div> -->
   </header>
   <div class="p-4">
-    <h2>Les boutons</h2>
-  <div>
-    <ButtonComponent type="primary">Bouton Principal</ButtonComponent>
+    <h2 class="text-5xl">Les boutons</h2>
+    <div>
+      <ButtonComponent type="primary">Bouton Principal</ButtonComponent>
       <ButtonComponent type="success">Bouton Succès</ButtonComponent>
       <ButtonComponent type="danger">Bouton Danger</ButtonComponent>
       <ButtonComponent type="secondary">Bouton Secondaire</ButtonComponent>
       <ButtonComponent>Bouton par défaut</ButtonComponent>
     </div>
     <div>
-      <h2>Les tires</h2>
-      <!-- <TitleComponent level="h1">Titre de niveau 1</TitleComponent>
-                                            <TitleComponent level="h2">Titre de niveau 2</TitleComponent>
-                                            <TitleComponent level="h3">Titre de niveau 3</TitleComponent> -->
+      <h2 class="text-5xl">Les titres</h2>
       <TitleComponent level="h1">Titre H1</TitleComponent>
       <TitleComponent level="h2">Titre H2</TitleComponent>
       <TitleComponent level="h3">Titre H3</TitleComponent>
@@ -44,9 +33,12 @@ import ContactFormComponent from '/src/components/ContactFormComponent.vue'
       <!-- Ajoutez d'autres instances avec différents niveaux au besoin -->
 
     </div>
+
+    <h2 class="text-5xl">Formulaire</h2>
     <ContactFormComponent>
 
     </ContactFormComponent>
+
   </div>
   <!-- <RouterView /> -->
 </template>
